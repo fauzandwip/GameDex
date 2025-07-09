8c17ac842e56f54278d805b364e2fc53550d399e
fauzandwip <fauzan27dp@gmail.com>
2025-07-09 21:36:29 +0700

    Initial Commit

---
+++ GameDex/GameDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameDexApp.swift
+//  GameDex
+//
+//  Created by Fauzan Dwi Prasetyo on 09/07/25.
+//
+
+import SwiftUI
+
+@main
+struct GameDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
